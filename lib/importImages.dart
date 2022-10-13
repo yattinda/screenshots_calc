16e7589591de4a5244f1aff1f9fd51cd8ba58e24
yattinda <yattinda@gmail.com>
2022-10-13 23:39:33 +0900

    Add pageroute

--- lib/importImages.dart
+++ lib/importImages.dart
@@ -1,23 +1,25 @@
-
 import 'package:flutter/material.dart';
+import 'package:screenshots_calc/info.dart';
 
-class importImages extends StatelessWidget {
+class ImportImages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Hello World"),
-          actions: [
-          IconButton(
-            icon: const Icon(Icons.info_outline),
-            onPressed: () => {},
-          )
-    ]
-      ),
+      appBar: AppBar(title: const Text("Hello World"), actions: [
+        IconButton(
+          icon: const Icon(Icons.info_outline),
+          onPressed: () => {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Info(),
+                ))
+          },
+        )
+      ]),
       body: Container(
-        margin : const EdgeInsets.only(right: 5, left: 5),
-        child: const Text('Initial')
-      ),
+          margin: const EdgeInsets.only(right: 5, left: 5),
+          child: const Text('Initial')),
     );
   }
 }
