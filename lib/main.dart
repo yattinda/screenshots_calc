4c9ab22c816d1d56595d61f333a8b651466063d5
yattinda <yattinda@gmail.com>
2022-10-13 23:20:49 +0900

    Use provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:screenshots_calc/changeTheme.dart';
 
 import 'importImages.dart';
+import 'changeTheme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      ProviderScope(
+        child: MyApp(),
+      ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyApp extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: importImages(),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.light(),
+        home: importImages(),
+      );
   }
 }
 
