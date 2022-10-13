78a1cb18e4ad024363229c509a0dc811a6527e4e
yattinda <yattinda@gmail.com>
2022-10-13 23:19:37 +0900

    TODO: change darkmode to use riverpod

---
+++ lib/changeTheme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// final countStateProvider = StateNotifierProvider((ref, current) => ChangeTheme());
+//
+// class ChangeTheme extends ChangeNotifier {
+//   ThemeData current = ThemeData.light();
+//   bool _isDark = false;
+//
+//   toggle() {
+//     _isDark = !_isDark;
+//     current = _isDark ? ThemeData.dark() : ThemeData.light();
+//   }
+// }
